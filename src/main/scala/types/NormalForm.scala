b20718d91ca95e8b77cf7e35e4c91f8c53d6feca
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-06-07 20:07:15 +0200

    Formal normal conj y disy

--- src/main/scala/types/NormalForm.scala
+++ src/main/scala/types/NormalForm.scala
@@ -8,7 +8,19 @@ object NormalForm {
     * Devuelve la proposición en forma normal negativa
     *
     */
-  def negative(prop: Prop) = insideNeg(removeImpl(removeEquiv(prop)))
+  def negative(prop: Prop) = takeInsideNeg(removeImpl(removeEquiv(prop)))
+
+  /**
+    * Devuelve la proposición en forma normal conjuntiva
+    *
+    */
+  def conjunctive(prop: Prop) = takeInsideDisj(negative(prop))
+
+  /**
+    * Devuelve la proposición en forma normal disyuntiva
+    *
+    */
+  def disjunctive(prop: Prop) = takeInsideConj(negative(prop))
 
   private def removeEquiv(prop: Prop): Prop = prop match {
     case p: Const => p
@@ -33,21 +45,55 @@ object NormalForm {
     case Impl(f, g) => Disj(Neg(removeImpl(f)), removeImpl(g))
   }
 
-  private def insideNeg(prop: Prop): Prop = prop match {
+  private def takeInsideNeg(prop: Prop): Prop = prop match {
     case p: Const => p
     case p: Atom => p
     case Neg(f) => toNegatedForm(f)
-    case Conj(f, g) => Conj(insideNeg(f), insideNeg(g))
-    case Disj(f, g) => Disj(insideNeg(f), insideNeg(g))
-    case Equi(f, g) => Equi(insideNeg(f), insideNeg(g))
-    case Impl(f, g) => Impl(insideNeg(f), insideNeg(g))
+    case Conj(f, g) => Conj(takeInsideNeg(f), takeInsideNeg(g))
+    case Disj(f, g) => Disj(takeInsideNeg(f), takeInsideNeg(g))
+    case Equi(f, g) => Equi(takeInsideNeg(f), takeInsideNeg(g))
+    case Impl(f, g) => Impl(takeInsideNeg(f), takeInsideNeg(g))
   }
 
   private def toNegatedForm(prop: Prop): Prop = prop match {
     case p: Const => Neg(p)
     case p: Atom => Neg(p)
-    case Neg(f) => insideNeg(f)
+    case Neg(f) => takeInsideNeg(f)
     case Conj(f, g) => Disj(toNegatedForm(f), toNegatedForm(g))
     case Disj(f, g) => Conj(toNegatedForm(f), toNegatedForm(g))
   }
-}
+
+  /**
+    * Interioriza Disj en una formula en forma normal negativa
+    *
+    * !!! prop debe estar en formal normal negativa
+    */
+  private def takeInsideDisj(prop: Prop): Prop = prop match {
+    case Disj(Conj(f1, f2), g) =>
+      val _g = takeInsideDisj(g)
+      val _f1 = takeInsideDisj(f1)
+      val _f2 = takeInsideDisj(f2)
+      Conj(Disj(_f1, _g), Disj(_f2, _g))
+
+    case Disj(f, Conj(g1, g2)) => takeInsideDisj(Disj(Conj(g1,g2), f))
+    case Disj(f,g) => Conj(takeInsideDisj(f), takeInsideDisj(g))
+    case _ => prop
+  }
+
+  /**
+    * Interioriza Conj en una formula en forma normal negativa
+    *
+    * !!! prop debe estar en formal normal negativa
+    */
+  private def takeInsideConj(prop: Prop): Prop = prop match {
+    case Conj(Disj(f1,f2), g) =>
+      val _g = takeInsideDisj(g)
+      val _f1 = takeInsideDisj(f1)
+      val _f2 = takeInsideDisj(f2)
+      Disj(Conj(_f1, _g), Conj(_f2, _g))
+
+    case Conj(f, Disj(g1,g2)) => takeInsideConj(Conj(Disj(g1,g2), f))
+    case Conj(f, g) => Disj(takeInsideConj(f), takeInsideConj(g))
+    case _ => prop
+  }
+ }
