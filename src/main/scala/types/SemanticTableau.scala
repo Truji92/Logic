cd2bfea8126a3a100c2b9d64a88788d4c55f183b
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-06-11 12:57:51 +0200

    Tableros semanticos

--- src/main/scala/types/SemanticTableau.scala
+++ src/main/scala/types/SemanticTableau.scala
@@ -49,7 +49,7 @@ object SemanticTableau {
     * @param prop
     * @return
     */
-  def components(prop: Prop) = prop match {
+  def components(prop: Prop): Set[Prop] = prop match {
     case Neg(Neg(f))      => Set(f)
     case Conj(f, g)       => Set(f, g)
     case Neg(Impl(f, g))  => Set(f, Neg(g))
@@ -61,22 +61,92 @@ object SemanticTableau {
     case Neg(Equi(f, g))  => Set(Conj(f, Neg(g)), Conj(Neg(f), g))
   }
 
+  /**
+    * Si todos las fórmulas de un conjunto son literales
+    * @param props
+    * @return
+    */
   def allLiterals(props: Iterable[Prop]) = props.forall(_.isLiteral)
 
   def hasContradiction(props: Iterable[Prop]) = props.exists(p => props.exists(_ == Neg(p)))
 
   /**
     * Expansión de un conjunto de fórmulas mediante doble negación
-    * @param props
+    *
     */
   def expDN(props: Iterable[Prop], prop: Prop) =
     Set(components(prop) ++ props.filterNot(_ == prop))
 
+  /**
+    * Expansión de un conjunto de fórmulas mediante expasión alfa
+    *
+    */
   def expAlfa(props: Iterable[Prop], prop: Prop) =
     Set(components(prop) ++ props.filterNot(_ == prop))
 
+  /**
+    * Expansión de un conjunto de fórmulas mediante expasión beta
+    *
+    */
   def expBeta(props: Iterable[Prop], prop: Prop) = {
     val _props = props.filterNot(_ == prop)
     components(prop) map (p => Set(p) ++ _props  )
   }
+
+  //@TODO ??? si llega al else && betas.isEmpty??
+  /**
+    *
+    * @param props
+    * @return
+    */
+  def successors(props: Iterable[Prop]) = {
+    lazy val doubleNegs = props.find(isDoubleNeg)
+    lazy val alfas = props.find(isAlfa)
+    lazy val betas = props.find(isBeta)
+
+    if (doubleNegs.nonEmpty) expDN(props, doubleNegs.get)
+    else if(alfas.nonEmpty) expAlfa(props, alfas.get)
+    else expBeta(props, betas.get)
+  }
+
+  /**
+    * Obtiene los modelos (en forma de conjunto de proposiciones) de un conjunto de fórmulas
+    * por el método de tableros semánticos
+    * @param props
+    * @return
+    */
+  def modelsByTableaux(props: Iterable[Prop]): Set[Set[Prop]] =
+    if (hasContradiction(props)) Set.empty
+    else if (allLiterals(props)) Set(props.toSet)
+    else successors(props) flatMap modelsByTableaux
+
+
+  /**
+    * Conjunto de modelos generales de props por el método de tableros semánticos
+    * @param props
+    * @return
+    */
+  def generalModels(props: Iterable[Prop]) = {
+    val models = modelsByTableaux(props)
+
+    models.filter(
+      m => !(models - m).exists(_.subsetOf(m))
+    )
+  }
+
+  /**
+    * Indica si una fórmula es un teorema por el método de tableros semanticos
+    * @param prop
+    * @return
+    */
+  def isTheoremByTableaux(prop: Prop) = modelsByTableaux(Set(Neg(prop))) isEmpty
+
+  /**
+    * Verifica si la fórmula prop es consecuencia lógica del conjunto de fórmulas props mediante tableros semánticos
+    * @param props
+    * @param prop
+    * @return
+    */
+  def isDeductibleByTableaux(props: Iterable[Prop], prop: Prop) =
+    modelsByTableaux(Set(Neg(prop)) ++ props).isEmpty
 }
---
+++ src/test/scala/TableauTests.scala
@@ -0,0 +1,152 @@
+import types.Types
+import Types._
+import org.scalatest.FunSuite
+import types.SemanticTableau._
+import scala.language.postfixOps
+
+/**
+  *
+  */
+class TableauTests extends FunSuite {
+
+  val p = Atom("p")
+  val q = Atom("q")
+  val r = Atom("r")
+  val s = Atom("s")
+  val p1 = Atom("p1")
+  val p2 = Atom("p2")
+
+  test("Doble Negacion 1") {
+    assert(
+      isDoubleNeg(no(no(p)))
+    )
+  }
+
+  test("Doble Negacion 2") {
+    assert(
+      ! isDoubleNeg(no(p -> q))
+    )
+  }
+
+  test("Components 1") {
+    assert(
+      components((p AND q) -> r) == Set(no(p AND q), r)
+    )
+  }
+
+  test("Components 2") {
+    assert(
+      components(no((p AND q) -> r)) == Set(p AND q, no(r))
+    )
+  }
+
+  test("Conjunto Literales 1") {
+    assert(
+      ! allLiterals(List(p -> q, no(r), r AND s, p))
+    )
+  }
+
+  test("Conjunto Literales 2") {
+    assert(
+      allLiterals(List(p, no(q), r))
+    )
+  }
+
+  test("Contradiccion") {
+    assert(
+      hasContradiction(List(r, p AND q, no(p AND q)))
+    )
+  }
+
+  test("Exp DN") {
+    assert(
+      expDN(List(p, no(no(q)), r), no(no(q))) == Set(Set(q,p,r))
+    )
+  }
+
+  test("Exp Alfa") {
+    assert(
+      expAlfa(List(q, p AND s, r), p AND s) == Set(Set(q,p,s,r))
+    )
+  }
+
+  test("Exp Beta") {
+    assert(
+      expBeta(List(q, p OR s, r), p OR s) == Set(Set(p, q, r), Set(s, q, r))
+    )
+  }
+
+  test("Sucesores 1") {
+    assert(
+      successors(List(q OR s, no(no(r)), p1 AND p2)) == Set(Set(r, q OR s, p1 AND p2))
+    )
+  }
+
+  test("Sucesores 2") {
+    assert(
+      successors(List(r, q OR s, p1 AND p2)) == Set(Set(p1, p2, r, q OR s))
+    )
+  }
+
+  test("Sucesores 3") {
+    assert(
+      successors(List(p1, p2, r, q OR s)) == Set(Set(q, p1, p2, r), Set(s, p1,p2,r))
+    )
+  }
+//  "modelosTab ej 1" ~:
+//    modelosTab [p --> q, no(q --> p)]
+//  ==> [[no p,q],[q,no p]],
+  test("Models By tab") {
+    assert(
+      modelsByTableaux(List(p -> q, no(q -> p))) == Set(Set(no(p), q), Set(q, no(p)))
+    )
+  }
+//  "modelosTab ej 2" ~:
+//    modelosTab [p --> q, no q --> no p]
+//  ==> [[q,no p],[no p],[q],[no p,q]],
+  test("Models By tab 2") {
+    assert(
+      modelsByTableaux(List(p -> q, no(q) -> no(p))) == Set(Set(no(p), q), Set(no(p)), Set(q), Set(q, no(p)))
+    )
+  }
+//  "modelosGenerales ej 1" ~:
+//    modelosGenerales [p --> q, no q --> no p]
+//  ==> [[no p],[q]],
+  test("General Models ") {
+    assert(
+      generalModels(List(p -> q, no(q) -> no(p))) == Set(Set(no(p)), Set(q))
+    )
+  }
+//  "esTeoremaPorTableros ej 1" ~:
+//    esTeoremaPorTableros (p --> p)
+//  ==> True,
+  test("theorem by tabs") {
+    assert(
+      isTheoremByTableaux(p -> p)
+    )
+  }
+//  "esTeoremaPorTableros ej 2" ~:
+//    esTeoremaPorTableros (p --> q)
+//  ==> False,
+  test("theorem by tabs 2") {
+    assert(
+     ! isTheoremByTableaux(p -> q)
+    )
+  }
+//  "esDeduciblePorTableros ej 1"
+//  esDeduciblePorTableros [p -->
+//    ==> True,
+  test("deducible by tabs") {
+    assert(
+      isDeductibleByTableaux(List(p -> q, q -> r), p -> r)
+    )
+  }
+//  "esDeduciblePorTableros ej 2"
+//  esDeduciblePorTableros [p -->
+//    ==> False
+  test("deducible by tabs 2") {
+    assert(
+      ! isDeductibleByTableaux(List(p -> q, q -> r), p <-> r)
+    )
+  }
+}
